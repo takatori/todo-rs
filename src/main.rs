1e3e7e7c1c3f9e8b6c7e1835e2c29bb2a702c776
takatori <takatorisatoshi@team-lab.com>
2017-02-15 23:06:46 +0900

    ✍️shakyou main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
+#![feature(plugin, custom_derive, custom_attribute)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+extern crate serd_json;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
+#[macro_use] extern crate serde_derive;
+extern crate rocket_contrib;
+extern crate r2d2;
+extern crate r2d2_diesel;
+
+mod task;
+mod db;
+
+use rocket::request::{Form, FlashMessage};
+use rocket::response::{Flash, Redirect};
+use rocket_contrib::Template;
+
+use task::Task;
+
+
+#[derive(Debug, Serialize)]
+struct Context<'a, 'b>{ msg: Option<(&'a str, &'b str)>, tasks: Vec<Task> }
+
+impl<'a, 'b> Context<'a, 'b> {
+    
+    pub fn err(conn: &db::Conn, msg: &'a str) -> Context<'static, 'a> {
+        Context{ msg: Some(("error", msg)), task: Task::all(conn) }
+    }
+
+    pub fn raw(conn: &db::Conn, msg: Option<(&'a str, &'b str)>) -> Context<'a, 'b> {
+        Context{ msg: msg, tasks: Task::all(conn) }
+    }
+}
+
+#[post("/", data = "<todo_form>")]
+fn new(todo_form: Form<Task>, conn: db::Conn) -> Flash<Redirect> {
+    
+    let todo = todo_form:into_inner();
+    
+    if todo.description.is_empty() {
+        Flash::error(Redirect::to("/"), "Description cannot be empty.")
+    } else if todo.insert(&conn) {
+        Flash::success(Redirect::to("/"), "Todo successfully added.")
+    } else {
+        Flash::error(Redirect::to("/"), "Woops! The server failed.")
+    }
+}
+
+#[put("/<id>")]
+fn toggle(id: i32, conn: db::Conn) -> Result<Redirect, Template> {
+    if Task::toggle_with_id(id, &conn) {
+        Ok(Redirect::to("/"))
+    } else {
+        Err(Teamplate::render("index", &Context::err(&conn, "Couldn't toggle task.")))
+    }
+}
+
+#[delete("/<id>")]
+fn delete(id: i32, conn: db::Conn) -> Result<Flash<Redirect>, Template> {
+    if Task::delete_with_id(id, &conn) {
+        Ok(Flash::success(Redirect::to("/"), "Todo was deleted."))
+    } else {
+        Err(Template::render("index", &Context::err(&conn, "Couldn't delete task.")))
+    }
+}
+
+#[get("/")]
+fn index(msg: Option<FlashMessage>, conn: db::Conn) -> Template {
+    Template::render("index", &match msg) {
+        Some(ref msg) => Context::raw(&conn, Some((msg.name(), msg.msg()))),
+        None => Context::raw(&conn, None),
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .manage(db::init_pool())
+        .mount("/", routes![index, static_files::all])
+        .mount("/todo/", routes![new, toggle, delete])
+        .lounch();
 }
