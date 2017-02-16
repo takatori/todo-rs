aff32032fd3517f363970f57f63fbd84012c6b6a
takatori <takatorisatoshi@team-lab.com>
2017-02-16 23:57:27 +0900

    ✍️ shakyou db

---
+++ src/db.rs
@@ -0,0 +1,48 @@
+use std::ops::Deref;
+
+use r2d2;
+use diesel::sqlite::SqliteConnnection;
+use r2d2_diesel::ConnectionManager;
+
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+
+pub type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
+
+pub const DATABASE_FILE: &'static str = env!("DATABASE_URL");
+
+pub fn init_pool() -> Pool {
+    let config = r2d2::Config::default();
+    let manager = ConnectionManager::<SqliteConnection>::new(DATABASE_FILE);
+    r2d2::Pool::new(config, manager).expect("db pool") // Unwraps a result, yielding the content of an Ok.
+}
+
+pub struct Conn(r2d2::PooledConnection<ConnectionManager<SqliteConnection>>);
+
+// deref coercions
+impl Deref for Conn {
+    type Target = SqliteConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
+impl<'a, 'r> FromRequest<'a, 'r> for Conn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Conn, ()> {
+        let pool = match <State<Pool> as FromRequest>::from_request(request) {
+            Outcome::Success(pool) => pool,
+            Outcome::Failure(e) => return Outcome::Failure(e),
+            Outcome::Forward(_) => return Outcome::Forward(()),
+        };
+
+        match pool.get() {
+            Ok(conn) => Outcome::Success(Connn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
