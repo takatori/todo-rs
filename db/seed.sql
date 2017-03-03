84d7ee8b6d99c2157a9279c50a389cf4ae6feaa5
takatori <takatorisatoshi@team-lab.com>
2017-03-03 23:18:26 +0900

    Remove seed

--- db/seed.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tasks (
-       id    INTEGER PRIMARY KEY,
-       description VACHAR NOT NULL,
-       completed BOOLEAN NOT NULL DEFAULT 0
-);
-
-INSERT INTO tasks (description) VALUES ("my first task");
